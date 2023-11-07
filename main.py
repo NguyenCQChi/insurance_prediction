ffe376b146b9295598f26a3816f9a65d28f140ad
Chi Nguyen <chinsu190103@gmail.com>
2023-11-07 13:39:08 -0800

    Add data visualization

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import pandas as pd
+
 import preprocessing_data as processor
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+
+# feature 1, 2, 6, 8, 10, 12, 17 for scatter
+
+COL_NUM = 3
 
 
 def split_data(data):
@@ -15,8 +23,48 @@ def split_data(data):
     return x_train, x_test, y_train, y_test
 
 
+def split_in_out(data):
+    X = data.drop(columns=['ClaimAmount'])
+    Y = pd.DataFrame(data['ClaimAmount'])
+    return X, Y
+
+
+def scatter(X, Y):
+    num_features = X.columns.size
+    fig, axs = plt.subplots(int(np.ceil(num_features / COL_NUM)), COL_NUM, figsize=(10, 10))
+    for i in range(num_features):
+        feature_name = X.columns[i]
+        feature = X[feature_name].values
+
+        scatter_plot = axs[int(np.floor(i / COL_NUM)), i % COL_NUM]
+        scatter_plot.scatter(feature, Y.values)
+        scatter_plot.set_xlabel(feature_name, size=10)
+        scatter_plot.set_ylabel(Y.columns[0], size=10)
+
+    fig.tight_layout(pad=2)
+    fig.show()
+
+
+# def hist(X, Y):
+#     num_features = X.columns.size
+#     fig, axs = plt.subplots(int(np.ceil(num_features / COL_NUM)), COL_NUM, figsize=(10, 10))
+#     for i in range(num_features):
+#         feature_name = X.columns[i]
+#         feature = X[feature_name].vaues
+#
+#         hist_axs = axs[int(np.floor(i / COL_NUM)), i % COL_NUM]
+#         hist_axs.hist(feature, bins=5)
+#         hist_axs.set_xlabel(feature_name, size=10)
+#
+#     fig.tight_layout(pad=2)
+#     fig.show()
+
+
 def main():
-    print("Hello World")
+    data = processor.process_data('trainingset.csv')
+    X, Y = split_in_out(data)
+    scatter(X, Y)
+    # hist(X, Y)
 
 
 if __name__ == "__main__":
